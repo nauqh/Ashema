79a9e36babb2d4160fb57d94bbc1fc5d5c3e287a
nauqh <hodominhquan.work@gmail.com>
2023-02-08 20:42:04 +0700

    Edit queue

--- ashema/extensions/music.py
+++ ashema/extensions/music.py
@@ -1,6 +1,8 @@
-import os
 from typing import Optional
+from datetime import datetime as dt
 from googleapiclient.discovery import build
+
+import os
 import random
 import hikari
 import lightbulb
@@ -12,19 +14,20 @@ plugin = lightbulb.Plugin("Music", "🎧 Music commands")
 # If True connect to voice with the hikari gateway instead of lavasnek_rs's
 HIKARI_VOICE = False
 
+
 class EventHandler:
     """Events from the Lavalink server"""
 
     async def track_start(self, lavalink: lavasnek_rs.Lavalink, event: lavasnek_rs.TrackStart) -> None:
         logging.info("Track started on guild: %s", event.guild_id)
-        
+
         node = await lavalink.get_guild_node(event.guild_id)
         # update bot's presence (should only work if bot is in one server)
         await plugin.bot.update_presence(
-            activity = hikari.Activity(
-            name = f"{node.now_playing.track.info.author} - {node.now_playing.track.info.title}",
-            type = hikari.ActivityType.LISTENING
-        ))
+            activity=hikari.Activity(
+                name=f"{node.now_playing.track.info.author} - {node.now_playing.track.info.title}",
+                type=hikari.ActivityType.LISTENING
+            ))
 
     async def track_finish(self, lavalink: lavasnek_rs.Lavalink, event: lavasnek_rs.TrackFinish) -> None:
         logging.info("Track finished on guild: %s", event.guild_id)
@@ -33,13 +36,14 @@ class EventHandler:
         # update bot's presence (should only work if bot is in one server)
         if not node.queue:
             await plugin.bot.update_presence(
-                activity = hikari.Activity(
-                name = f"/play",
-                type = hikari.ActivityType.LISTENING
-            ))
+                activity=hikari.Activity(
+                    name=f"/play",
+                    type=hikari.ActivityType.LISTENING
+                ))
 
     async def track_exception(self, lavalink: lavasnek_rs.Lavalink, event: lavasnek_rs.TrackException) -> None:
-        logging.warning("Track exception event happened on guild: %d", event.guild_id)
+        logging.warning(
+            "Track exception event happened on guild: %d", event.guild_id)
 
         # If a track was unable to be played, skip it
         skip = await lavalink.skip(event.guild_id)
@@ -52,6 +56,8 @@ class EventHandler:
             await lavalink.stop(event.guild_id)
 
 # on ready, connect to lavalink server
+
+
 @plugin.listener(hikari.ShardReadyEvent)
 async def start_lavalink(event: hikari.ShardReadyEvent) -> None:
     # local lavalink server
@@ -69,7 +75,8 @@ async def start_lavalink(event: hikari.ShardReadyEvent) -> None:
     plugin.bot.d.lavalink = lava_client
     plugin.bot.unsubscribe(hikari.ShardReadyEvent, start_lavalink)
 
-    youtube = build('youtube', 'v3', static_discovery=False, developerKey=os.environ["YOUTUBE_API_KEY"])
+    youtube = build('youtube', 'v3', static_discovery=False,
+                    developerKey=os.environ["YOUTUBE_API_KEY"])
     plugin.bot.d.youtube = youtube
 
 
@@ -78,8 +85,10 @@ async def _join(ctx: lightbulb.Context) -> Optional[hikari.Snowflake]:
 
     states = plugin.bot.cache.get_voice_states_view_for_guild(ctx.guild_id)
 
-    voice_state = [state[1] for state in filter(lambda i : i[0] == ctx.author.id, states.items())]
-    bot_voice_state = [state[1] for state in filter(lambda i: i[0] == ctx.bot.get_me().id, states.items())]
+    voice_state = [state[1] for state in filter(
+        lambda i: i[0] == ctx.author.id, states.items())]
+    bot_voice_state = [state[1] for state in filter(
+        lambda i: i[0] == ctx.bot.get_me().id, states.items())]
 
     if not voice_state:
         await ctx.respond("Connect to a voice channel first.")
@@ -110,6 +119,7 @@ async def _join(ctx: lightbulb.Context) -> Optional[hikari.Snowflake]:
 
     return channel_id
 
+
 async def _play(ctx: lightbulb.Context, query: str):
     assert ctx.guild_id is not None
 
@@ -132,11 +142,12 @@ async def _play(ctx: lightbulb.Context, query: str):
         return
 
     await ctx.respond(
-        embed = hikari.Embed(
-            description = f"[{query_information.tracks[0].info.title}]({query_information.tracks[0].info.uri}) added to queue [{ctx.author.mention}]",
-            colour = 0x76ffa1
+        embed=hikari.Embed(
+            description=f"[{query_information.tracks[0].info.title}]({query_information.tracks[0].info.uri}) added to queue [{ctx.author.mention}]",
+            colour=0x76ffa1
         )
     )
+
 
 @plugin.command()
 @lightbulb.add_checks(lightbulb.guild_only)
@@ -147,6 +158,7 @@ async def join(ctx: lightbulb.Context) -> None:
 
     if channel_id:
         await ctx.respond(f"Joined <#{channel_id}>")
+
 
 @plugin.command()
 @lightbulb.add_checks(lightbulb.guild_only)
@@ -170,16 +182,18 @@ async def leave(ctx: lightbulb.Context) -> None:
 
     await ctx.respond("Left voice channel")
 
+
 @plugin.command()
 @lightbulb.add_checks(lightbulb.guild_only)
 @lightbulb.option("query", "The query to search for.", modifier=lightbulb.OptionModifier.CONSUME_REST, required=True)
-@lightbulb.command("play", "Searches the query on youtube, or adds the URL to the queue.", auto_defer = True)
+@lightbulb.command("play", "Searches the query on youtube, or adds the URL to the queue.", auto_defer=True)
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 async def play(ctx: lightbulb.Context) -> None:
     """Searches the query on youtube, or adds the URL to the queue."""
 
     query = ctx.options.query
     await _play(ctx, query)
+
 
 @plugin.command()
 @lightbulb.add_checks(lightbulb.guild_only)
@@ -193,13 +207,14 @@ async def stop(ctx: lightbulb.Context) -> None:
     node.queue = []
     await plugin.bot.d.lavalink.set_guild_node(ctx.guild_id, node)
     skip = await plugin.bot.d.lavalink.skip(ctx.guild_id)
-    
+
     await ctx.respond(
-        embed = hikari.Embed(
-            description = ":stop_button: Stopped playing",
-            colour = 0xd25557
+        embed=hikari.Embed(
+            description=":stop_button: Stopped playing",
+            colour=0xd25557
         )
     )
+
 
 @plugin.command()
 @lightbulb.add_checks(lightbulb.guild_only)
@@ -220,12 +235,13 @@ async def skip(ctx: lightbulb.Context) -> None:
             await plugin.bot.d.lavalink.stop(ctx.guild_id)
 
         await ctx.respond(
-            
-            embed = hikari.Embed(
-                description =   f":fast_forward: Skipped: [{skip.track.info.title}]({skip.track.info.uri})",
-                colour = 0xd25557
+
+            embed=hikari.Embed(
+                description=f":fast_forward: Skipped: [{skip.track.info.title}]({skip.track.info.uri})",
+                colour=0xd25557
             )
         )
+
 
 @plugin.command()
 @lightbulb.add_checks(lightbulb.guild_only)
@@ -236,11 +252,12 @@ async def pause(ctx: lightbulb.Context) -> None:
 
     await plugin.bot.d.lavalink.pause(ctx.guild_id)
     await ctx.respond(
-        embed = hikari.Embed(
-            description = ":pause_button: Paused player",
-            colour = 0xf9c62b
+        embed=hikari.Embed(
+            description=":pause_button: Paused player",
+            colour=0xf9c62b
         )
     )
+
 
 @plugin.command()
 @lightbulb.add_checks(lightbulb.guild_only)
@@ -251,45 +268,52 @@ async def resume(ctx: lightbulb.Context) -> None:
 
     await plugin.bot.d.lavalink.resume(ctx.guild_id)
     await ctx.respond(
-        embed = hikari.Embed(
-            description = ":arrow_forward: Resumed player",
-            colour = 0x76ffa1
+        embed=hikari.Embed(
+            description=":arrow_forward: Resumed player",
+            colour=0x76ffa1
         )
     )
 
+
 @plugin.command()
 @lightbulb.add_checks(lightbulb.guild_only)
-@lightbulb.command("queue", "Shows the next 10 songs in the queue", aliases = ['q'])
+@lightbulb.command("queue", "Shows the next 10 songs in the queue", aliases=['q'])
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
-async def queue(ctx : lightbulb.Context) -> None:
+async def queue(ctx: lightbulb.Context) -> None:
     node = await plugin.bot.d.lavalink.get_guild_node(ctx.guild_id)
+    target = ctx.get_guild().get_member(ctx.user)
 
     if not node or not node.queue:
         await ctx.respond("Nothing is playing at the moment.")
         return
-    
+
     length = divmod(node.now_playing.track.info.length, 60000)
     queueDescription = f"Now playing: [{node.now_playing.track.info.title}]({node.now_playing.track.info.uri}) `{int(length[0])}:{round(length[1]/1000):02}` [<@!{node.now_playing.requester}>]"
     i = 1
     while i < len(node.queue) and i <= 10:
-        if i == 1: 
+        if i == 1:
             queueDescription += '\n\nUp next:'
         length = divmod(node.queue[i].track.info.length, 60000)
-        queueDescription = queueDescription + f"\n[{i}. {node.queue[i].track.info.title}]({node.queue[i].track.info.uri}) `{int(length[0])}:{round(length[1]/1000):02}` [<@!{node.queue[i].requester}>]"
+        queueDescription = queueDescription + \
+            f"\n[{i}. {node.queue[i].track.info.title}]({node.queue[i].track.info.uri}) `{int(length[0])}:{round(length[1]/1000):02}` [<@!{node.queue[i].requester}>]"
         i += 1
 
     queueEmbed = hikari.Embed(
-        title = "Queue",
-        description = queueDescription,
-        colour = 0x76ffa1
-    )
+        title="🎶 Queue",
+        description=queueDescription,
+        colour=0x181818,
+        timestamp=dt.now().astimezone()
+    ).set_footer(
+        text=f"Requested by {ctx.member.display_name}",
+        icon=ctx.member.avatar_url or ctx.member.default_avatar_url,
+    ).set_thumbnail(target.avatar_url or target.default_avatar_url)
 
-    await ctx.respond(embed = queueEmbed)
+    await ctx.respond(embed=queueEmbed)
 
 
 @plugin.command()
 @lightbulb.add_checks(lightbulb.guild_only)
-@lightbulb.command("chill", "Play random linhnhichill", auto_defer = True)
+@lightbulb.command("chill", "Play random linhnhichill", auto_defer=True)
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 async def chill(ctx: lightbulb.Context) -> None:
 
@@ -302,15 +326,16 @@ async def chill(ctx: lightbulb.Context) -> None:
         res = plugin.bot.d.youtube.playlistItems().list(
             playlistId='PL-F2EKRbzrNS0mQqAW6tt75FTgf4j5gjS',  # linhnhichill's playlist ID
             part='snippet',
-            pageToken = next_page_token,
+            pageToken=next_page_token,
             maxResults=50
         ).execute()
 
         if rand_vid == -1:
             rand_vid = random.randint(0, res['pageInfo']['totalResults'])
         if rand_vid < 50:
-            vid_id = res['items'][rand_vid]['snippet']['resourceId']['videoId']  # id
-            query = f"{BASE_YT_URL}?v={vid_id}" 
+            # id
+            vid_id = res['items'][rand_vid]['snippet']['resourceId']['videoId']
+            query = f"{BASE_YT_URL}?v={vid_id}"
             break
 
         rand_vid -= 50
@@ -334,24 +359,27 @@ async def voice_state_update(event: hikari.VoiceStateUpdateEvent) -> None:
             cur_state.channel_id,
         )
 
-    # if only one users using bot, bot pauses when user deafens and 
+    # if only one users using bot, bot pauses when user deafens and
     # resumes when user undeafen
 
-    if cur_state.user_id == plugin.bot.get_me().id:  # bot 
+    if cur_state.user_id == plugin.bot.get_me().id:  # bot
         return
 
-    states = plugin.bot.cache.get_voice_states_view_for_guild(cur_state.guild_id).items()
-    bot_voice_state = [state[1] for state in filter(lambda i: i[0] == plugin.bot.get_me().id, states)]
+    states = plugin.bot.cache.get_voice_states_view_for_guild(
+        cur_state.guild_id).items()
+    bot_voice_state = [state[1] for state in filter(
+        lambda i: i[0] == plugin.bot.get_me().id, states)]
 
     # bot not in voice channel or user not in the same channel as bot
     if not bot_voice_state or cur_state.channel_id != bot_voice_state[0].channel_id:
         return
-    
+
     # count users in channel with bot
-    cnt_user = len([state[0] for state in filter(lambda i: i[1].channel_id == bot_voice_state[0].channel_id, states)])
+    cnt_user = len([state[0] for state in filter(
+        lambda i: i[1].channel_id == bot_voice_state[0].channel_id, states)])
     if cnt_user != 2:  # if not just bot & user
         return
-    
+
     if prev_state.is_self_deafened and not cur_state.is_self_deafened:
         await plugin.bot.d.lavalink.resume(cur_state.guild_id)
         logging.info("Track resumed on guild: %s", event.guild_id)
